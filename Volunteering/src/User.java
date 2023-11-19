74b11824a4b7a4e5e181d75b0f4d7fb64f72938e
Shiras54 <HungerGamesPro54@gmail.com>
2023-11-19 10:45:05 +0400

    worked on setter methods, made createID method

--- Volunteering/src/User.java
+++ Volunteering/src/User.java
@@ -1,17 +1,20 @@
 import java.util.*;
+import java.text.*;
 
 public class User {
-	private String id, name, dob, email, phone, address, password;
+	private String id, name, email, phone, address, password;
 	private List<Initiative> volunteeringJobs = new ArrayList<Initiative>();
 	private Initiative initiative1,initiative2;
 	private int points;
+	private Date dob;
+	private static int idCounter;
 	
 	public User() {
 		
 	}
 	
 	public User(String name, String dob, String email, String phone, String address, String password) {
-		setId();
+		createID();
 		setName(name);
 		setDob(dob);
 		setEmail(email);
@@ -65,22 +68,50 @@ public class User {
 		}
 	}
 	
+	public void createID() {
+		idCounter++;
+		this.id = String.format("%10.0f", idCounter);
+	}
+	
 	public String getId() {return id;}
-	public void setId() {this.id = "";}
+	public void setId(String id) {
+		this.id = id;
+		}
 	public String getName() {return name;}
-	public void setName(String name) {this.name = name;}
-	public String getDob() {return dob;}
-	public void setDob(String dob) {this.dob = dob;}
+	public void setName(String name) {
+		if(name.contains("")) {	
+			this.name = name;
+		}
+	}	
+	public Date getDob() {return dob;}
+	public void setDob(String dob) {
+		try {
+			DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+			this.dob = format.parse(dob);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
 	public String getEmail() {return email;}
-	public void setEmail(String email) {this.email = email;}
+	public void setEmail(String email) {
+		if(email.contains("@")&&email.contains(".")) {
+			this.email = email;
+		}
+	}
 	public String getPhone() {return phone;}
 	public void setPhone(String phone) {this.phone = phone;}
 	public String getAddress() {return address;}
-	public void setAddress(String address) {this.address = address;}
+	public void setAddress(String address) {
+		this.address = address;
+		}
 	public String getPassword() {return password;}
-	public void setPassword(String password) {this.password = password;}
+	public void setPassword(String password) {
+		this.password = password;
+		}
 	public Initiative getInitiative1() {return initiative1;}
-	public void setInitiative1(Initiative initiative) {this.initiative1 = initiative;}
+	public void setInitiative1(Initiative initiative) {
+		this.initiative1 = initiative;
+		}
 	public List<Initiative> getVolunteeringJobs() {return volunteeringJobs;}
 	public void setVolunteeringJobs(List<Initiative> volunteeringJobs) {this.volunteeringJobs = volunteeringJobs;}
 	public int getPoints() {return points;}
