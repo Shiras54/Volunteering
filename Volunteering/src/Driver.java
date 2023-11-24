426cfea66d5c15e9524c94b7ccfe0103d3cc2f22
Shiras54 <HungerGamesPro54@gmail.com>
2023-11-24 15:53:08 +0400

    fix

--- Volunteering/src/Driver.java
+++ Volunteering/src/Driver.java
@@ -1,5 +1,4 @@
 public class Driver {
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		Initiative.pendingInitiatives.add(new Initiative("000","0","01/01/0001 00:00:00","pending","000",5,2,new User()));
 		Initiative.activeInitiatives.add(new Initiative("000","0","01/01/0001 00:00:00","active","000",5,2,new User()));
@@ -7,9 +6,5 @@ public class Driver {
 		User u = new User();
 		u.initiate();	
 		new Page1();
-=======
-	public static void main(String[] args) {	
-		 new Page1();
->>>>>>> branch 'master' of https://github.com/Shiras54/Volunteering.git
 	}
 }
